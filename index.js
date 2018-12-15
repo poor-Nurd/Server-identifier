4b8c8487e0d67eac77620a88ac35fa86a1cfaa34
poor-Nurd <45834983+poor-Nurd@users.noreply.github.com>
2018-12-15 18:11:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+module.exports = function ServerIdentifier(mod) { //Nurd#1025
+	let enabled = true;
+	let zone;
+	let S = { 
+					26:' :  Killian  : ' ,
+					27:' :  Mystel  : '  ,
+					28:' :  Yurian  : ' ,
+					29:' :  Seren  : ' ,
+			};
+
+	mod.command.add(['si','ServerIdentifier'], {
+		$none() {
+			enabled = !enabled
+			mod.command.message(`       <font color="#954193">Server Identifier : </font> <font color="#ffff00">${enabled ? 'Enabled' : 'Disabled'} </font> `)
+		}
+	})	
+	
+
+	mod.hook('S_LOAD_TOPO', 3, (event) => {
+        zone = event.zone;
+    });
+	
+	mod.hook('S_SPAWN_USER',13, event =>{
+		if(enabled)
+		{		
+			if(zone >= 9000)
+			{
+				if (S[event.serverId])
+				{
+					event.guild = ' '+event.guild +S[event.serverId]+' '
+					return true;
+				}
+			}	
+		}
+		
+	
+	})
+	
+
+	
+
+	
+	
+	
+	
+	
+	
+}
